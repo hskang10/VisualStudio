4c7b8baaafd7e50bdc9cb12a0a46477aa740a8f2
Hansu Kang <khs1810@naver.com>
2015-12-12 01:39:31 +0900

    s

--- SensorNetworkManager_WPF/SensorNetworkManager_WPF/SubWindow.xaml.cs
+++ SensorNetworkManager_WPF/SensorNetworkManager_WPF/SubWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Security.RightsManagement;
 using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
@@ -14,25 +16,54 @@ namespace SensorNetworkManager_WPF {
 	/// </summary>
 	public partial class SubWindow : Window {
 
+		public class Node {
+			public int Id { get; }
+			public int Level { get; set; }
+
+			public Node() {}
+
+			public Node(int Id, int level) {
+				this.Id = Id;
+				this.Level = level;
+			}
+		}
+
+		public class NodePoint : Node {
+			public Ellipse Circle { get; set; }
+			public RadioButton Button { get; set; }
+			public BackgroundWorker Worker { get; set; }
+
+			public NodePoint() : base() {}
+
+			public NodePoint(int Id, int level) : base(Id, level) {}
+		}
+
+		private class NodeList : List<NodePoint> {}
+
+		NodeList nodeList;
+		bool _isSet = false;
+
+		/*
 		private struct Node {
 			public byte ID;
 			public byte level;
-		}
+		}*/
 
 		private MainWindow mainWindow;
-
 		private int _count = 0;
-
 		private const int ID_OFFSET = 0x01;
 
+		/*
 		private Ellipse[] ellipse;
 		private RadioButton[] button;
 		private BackgroundWorker[] backgroundWorker;
 		private Node[] node;
+		*/
 
 		public SubWindow(MainWindow mainW) {
 			mainWindow = mainW;
 			InitializeComponent();
+			this.nodeList = new NodeList();
 		}
 
 		private void Window_Loaded(object sender, RoutedEventArgs e) {
@@ -47,28 +78,50 @@ namespace SensorNetworkManager_WPF {
 		/// </summary>
 		/// <param name="packet">The packet.</param>
 		public void Blink(Packet packet) {
-			byte srcID = packet.sourceID;
-			int idx = packet.sourceID - ID_OFFSET;
-			
+			if (_isSet) {
+				int idx = packet.sourceID - ID_OFFSET;
 
-			Dispatcher.Invoke(new Action(delegate {
-				button[idx].FontWeight = FontWeights.Bold;
-				button[idx].Content = packet.sourceLevel;
-			}));
 
-			node[idx].level = packet.sourceLevel;
+				Dispatcher.Invoke(new Action(delegate {
+					this.nodeList[idx].Button.FontWeight = FontWeights.Bold;
+					this.nodeList[idx].Button.Content = packet.sourceLevel;
+					/*
+					button[idx].FontWeight = FontWeights.Bold;
+					button[idx].Content = packet.sourceLevel;
+					*/
+				}));
 
+				this.nodeList[idx].Level = packet.sourceLevel;
+
+				if (!this.nodeList[idx].Worker.IsBusy)
+					this.nodeList[idx].Worker.RunWorkerAsync(idx);
+			}
+
+
+			//node[idx].level = packet.sourceLevel;
+
+			/*
 			if (!backgroundWorker[idx].IsBusy)
 				backgroundWorker[idx].RunWorkerAsync(idx);
+				*/
+
+
 		}
 
 		public void Set_Node() {
+			_isSet = false;
 			_count = 0;
 			int num = mainWindow.numOfNodes;
+			
+			for(int i =0; i<num; i++)
+				this.nodeList.Add(new NodePoint(i + ID_OFFSET, 0));
+			MessageBox.Show(this.nodeList.Count.ToString());
+			/*
 			ellipse = new Ellipse[num];
 			button = new RadioButton[num];
 			backgroundWorker = new BackgroundWorker[num];
 			node = new Node[num];
+			*/
 			this.grid.MouseLeftButtonDown += Grid_MouseLeftButtonDown;
 			label.Content = "0x" + (_count + 1).ToString("X2") + " 위치를 지정하세요.";
 		}
@@ -77,6 +130,13 @@ namespace SensorNetworkManager_WPF {
 			if (_count < mainWindow.numOfNodes) {
 				Point p = e.GetPosition(grid);
 
+				this.nodeList[this._count].Circle = CreateEllipse(p);
+				this.grid.Children.Add(this.nodeList[_count].Circle);
+				this.nodeList[this._count].Button = CreateButton(p);
+				this.grid.Children.Add(this.nodeList[_count].Button);
+				this.nodeList[this._count].Worker = new BackgroundWorker();
+				this.nodeList[this._count].Worker.DoWork += this.worker_DoWork;
+				/*
 				ellipse[_count] = CreateEllipse(p);
 				this.grid.Children.Add(ellipse[_count]);
 
@@ -85,6 +145,7 @@ namespace SensorNetworkManager_WPF {
 
 				backgroundWorker[_count] = new BackgroundWorker();
 				backgroundWorker[_count].DoWork += new DoWorkEventHandler(worker_DoWork);
+				*/
 				_count++;
 
 				label.Content = "0x" + (_count + 1).ToString("X2") + " 위치를 지정하세요.";
@@ -94,12 +155,14 @@ namespace SensorNetworkManager_WPF {
 					mainWindow._isSetNode = true;
 					this.grid.MouseLeftButtonDown -= Grid_MouseLeftButtonDown;
 					MessageBox.Show("설정 완료");
+					this._isSet = true;
 				}
 			}
 		}
 
 		private void SubWindow_Click(object sender, RoutedEventArgs e) {
 			for (int i = 0; i < mainWindow.numOfNodes; i++) {
+				/*
 				if (button[i].GetHashCode() == sender.GetHashCode()) {
 					this.label_ID.Content = "0x" + (i + ID_OFFSET).ToString("X2");
 
@@ -107,6 +170,15 @@ namespace SensorNetworkManager_WPF {
 						this.label_level.Content = "알 수 없음";
 					else
 						this.label_level.Content = node[i].level;
+				}*/
+
+				if (this.nodeList[i].Button.GetHashCode() == sender.GetHashCode()) {
+					this.label_ID.Content = "0x" + (i + ID_OFFSET).ToString("X2");
+
+					if(this.nodeList[i].Level < 1)
+						this.label_level.Content = "알 수 없음";
+					else
+						this.label_level.Content = nodeList[i].Level;
 				}
 			}
 		}
@@ -117,13 +189,15 @@ namespace SensorNetworkManager_WPF {
 			for (int j = 0; j < 2; j++) {
 				for (int i = 0; i < 100; i++) {
 					Dispatcher.Invoke(new Action(delegate () {
-						ellipse[index].Opacity = (double)i / 100;
+						this.nodeList[index].Circle.Opacity = (double) i/100;
+						//ellipse[index].Opacity = (double)i / 100;
 					}));
 					Thread.Sleep(1);
 				}
 				for (int i = 100; i > 0; i--) {
 					Dispatcher.Invoke(new Action(delegate () {
-						ellipse[index].Opacity = (double)i / 100;
+						this.nodeList[index].Circle.Opacity = (double)i / 100;
+						//ellipse[index].Opacity = (double)i / 100;
 					}));
 					Thread.Sleep(1);
 				}
